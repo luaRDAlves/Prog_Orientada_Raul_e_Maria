1a461282e46fd70faec85b203a3b473dedb8c3f1
Hugo Marcondes <hugo.marcondes@ifsc.edu.br>
2025-08-27 15:29:49 -0300

    Container updated

--- toolbox/Container/setup_extensions.sh
+++ toolbox/Container/setup_extensions.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
 code --install-extension cantonios.project-templates
-code --install-extension dnut.rewrap-revived
-code --install-extension filipkarlsson.cxx-tools
-code --install-extension fleexo.cpp-class-creator
-code --install-extension mhutchie.git-graph
 code --install-extension ms-vscode.cmake-tools
 code --install-extension ms-vscode.cpptools
 code --install-extension ms-vscode.cpptools-extension-pack
-code --install-extension ms-vscode.cpptools-themes
+code --install-extension amiralizadeh9480.cpp-helper
+code --install-extension fleexo.cpp-class-creator
 code --install-extension theumletteam.umlet
+code --install-extension dnut.rewrap-revived
+code --install-extension mhutchie.git-graph
 
-tar -C / -zxvf /install/code-templates.tgz
+tar -C /usr/share/code/data/user-data/User/ -zxvf /install/code-templates.tgz
 
